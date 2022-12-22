dd193f1f0fd87bc715510832af2b8e5174031451
Nicholas de Paola <ndepaola97@gmail.com>
2022-12-22 22:31:11 +1000

    2022 day 21 brute-forced. need to revisit this and solve it properly.

--- years/2022/21/2022_day_21.py
+++ years/2022/21/2022_day_21.py
@@ -1,3 +1,8 @@
+"""
+TODO: I hacked this one together by reducing the data down to minimal equations and plugging it into an equation solver.
+Revise this and solve it properly.
+"""
+
 import operator
 import re
 from typing import Callable, TypeAlias
@@ -5,6 +10,14 @@ from typing import Callable, TypeAlias
 Monkeys: TypeAlias = dict[str, int]  # monkey name -> the number the monkey yells out
 MonkeyDependencies: TypeAlias = dict[str, tuple[str, str]]
 DependentMonkeys: TypeAlias = dict[str, Callable[[int, int], int]]
+
+OPS = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+    # "/": operator.truediv,
+    "/": operator.floordiv,
+}
 
 
 def read_input_file(file_name: str = "input.txt") -> tuple[Monkeys, MonkeyDependencies, DependentMonkeys]:
@@ -20,12 +33,7 @@ def read_input_file(file_name: str = "input.txt") -> tuple[Monkeys, MonkeyDepend
                 else:
                     op_string = [x for x in ["+", "-", "*", "/"] if x in rhs].pop()
                     monkey_1, monkey_2 = rhs.split(f" {op_string} ")
-                    dependent_monkeys[lhs] = {
-                        "+": operator.add,
-                        "-": operator.sub,
-                        "*": operator.mul,
-                        "/": operator.truediv,
-                    }[op_string]
+                    dependent_monkeys[lhs] = OPS[op_string]
                     monkey_dependencies[lhs] = (monkey_1, monkey_2)
         return monkeys, monkey_dependencies, dependent_monkeys
 
@@ -63,9 +71,9 @@ def quick_test(file_name: str = "input.txt"):
         for line in f.readlines():
             if stripped_line := line.strip():
                 lhs, rhs = stripped_line.split(": ")
-                lhs_and_rhs[lhs] = int(rhs) if rhs.isdigit() else rhs
+                lhs_and_rhs[lhs] = int(rhs) if rhs.isdigit() else rhs  # .replace("/", "//")
         print(lhs_and_rhs)
-        equation_with_humn = [(key, value) for key, value in lhs_and_rhs.items() if "humn" in value].pop()
+        # equation_with_humn = [(key, value) for key, value in lhs_and_rhs.items() if "humn" in value].pop()
         # TODO
         # lhs_and_rhs.pop(equation_with_humn[0])
         equation_with_humn = "ptdq + dvpt"
@@ -83,23 +91,67 @@ def quick_test(file_name: str = "input.txt"):
                     for thing in equations_that_this_key_is_in:
                         if thing not in ["root", "humn"]:
                             lhs_and_rhs[thing] = lhs_and_rhs[thing].replace(key, f"({value})")
-                            try:
-                                lhs_and_rhs[thing] = eval(lhs_and_rhs[thing])
-                            except NameError:
-                                result = re.search(r"\((\w{4}) ([+\-*\/]) (\w{4})\)", lhs_and_rhs[thing])
-                                print("")
-                                if result is not None:
-                                    ...  # TODO: continue this train of thought
-                                    # result.groups()[0], result.groups()[1], result.groups()[2],
-                                    # lhs_and_rhs[thing].replace(result.string[result.pos:result.endpos], "")
+                            # try:
+                            #     lhs_and_rhs[thing] = eval(lhs_and_rhs[thing])
+                            # except NameError:
+                            # result = re.search(r"\((\w{4}) ([+\-*\/]) (\w{4})\)", lhs_and_rhs[thing])
+                            # print("")
+                            # if result is not None and isinstance(lhs_and_rhs[thing], str) and isinstance(number_1 := lhs_and_rhs[result.groups()[0]], int) and isinstance(number_2 := lhs_and_rhs[result.groups()[2]], int):
+                            #     if result.groups()[0] != "humn" and result.groups()[2] != "humn":
+                            #         lhs_and_rhs[thing] = lhs_and_rhs[thing].replace(result.string[result.span()[0]:result.span()[1]], str(OPS[result.groups()[1]](number_1, number_2)))
+                            #         print("")
+                            result = re.search(r"\((\w{4}) ([+\-*\/]) (\w{4})\)", lhs_and_rhs[thing])
+                            if (
+                                result is not None
+                                and isinstance(lhs_and_rhs[thing], str)
+                                and isinstance(number_1 := lhs_and_rhs[result.groups()[0]], int)
+                                and isinstance(number_2 := lhs_and_rhs[result.groups()[2]], int)
+                            ):
+                                if result.groups()[0] != "humn" and result.groups()[2] != "humn":
+                                    lhs_and_rhs[thing] = lhs_and_rhs[thing].replace(
+                                        result.string[result.span()[0] : result.span()[1]],
+                                        str(OPS[result.groups()[1]](number_1, number_2)),
+                                    )
+                                    print("")
+
+                            # attempt to simplify
+                while True:
+                    # result = re.search(r"\(\(?(\d+)\)? ([+\-*\/]) \(?(\d+)\)?\)", str(lhs_and_rhs[key]))
+                    result = re.search(r"\([0-9\+\-\*\/\s\.]+\)", str(lhs_and_rhs[key]))
+                    print("")
+                    if result is None:
+                        break
+                    # lhs_and_rhs[key] = lhs_and_rhs[key].replace(result.string[result.span()[0]:result.span()[1]], str(OPS[result.groups()[1]](int(result.groups()[0]), int(result.groups()[2]))))
+                    lhs_and_rhs[key] = lhs_and_rhs[key].replace(
+                        result.string[result.span()[0] : result.span()[1]],
+                        str(eval(result.string[result.span()[0] : result.span()[1]])),
+                    )
+                    # if result is not None and isinstance(lhs_and_rhs[thing], str) and isinstance(
+                    #         number_1 := lhs_and_rhs[result.groups()[0]], int) and isinstance(
+                    #         number_2 := lhs_and_rhs[result.groups()[2]], int):
+                    #     if result.groups()[0] != "humn" and result.groups()[2] != "humn":
+                    #         lhs_and_rhs[thing] = lhs_and_rhs[thing].replace(
+                    #             result.string[result.span()[0]:result.span()[1]],
+                    #             str(OPS[result.groups()[1]](number_1, number_2)))
+                    #         print("")
+
+                    # result.groups()[0], result.groups()[1], result.groups()[2], OPS[result.groups()[1]](
+                    #     lhs_and_rhs[result.groups()[0]], lhs_and_rhs[result.groups()[2]])
+                    # ...  # TODO: continue this train of thought
+                    # result.groups()[0], result.groups()[1], result.groups()[2],
+                    # lhs_and_rhs[thing].replace(result.string[result.pos:result.endpos], "")
                 print("")
-                dumb = re.sub(
-                    r"\((\d+)\)", "\g<1>", f"{lhs_and_rhs['brrs']} = {lhs_and_rhs['fcjl']}".replace("humn", "x")
-                )
+                # [(result := re.search(r"[a-zA-Z]", str(lhs_and_rhs[x]))) is None or "humn" in result.string for x in
+                #  lhs_and_rhs.keys() if x not in lhs_and_rhs["root"].split(" + ")]
+
+                # dumb = re.sub(
+                #     r"\((\d+)\)", "\g<1>", f"{lhs_and_rhs['brrs']} = {lhs_and_rhs['fcjl']}".replace("humn", "x")
+                # )
 
 
 if __name__ == "__main__":
     m, m_d, d_m = read_input_file("input.txt")
     # print(f"The monkey named `root` will yell the number {resolve_monkeys(m, m_d, d_m)}.")
     # determine_what_number_to_yell(m, m_d, d_m)
+    # quick_test("example.txt")
     quick_test("input.txt")
