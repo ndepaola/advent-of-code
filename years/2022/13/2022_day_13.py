7910bd01715c2cbb4ef2abd3f23cf84afbbfdaf7
Nicholas de Paola <3079166+ndepaola@users.noreply.github.com>
2022-12-30 23:43:53 +1000

    2022 day 13 bugfix - didn't handle lists of identical length properly :(

--- years/2022/13/2022_day_13.py
+++ years/2022/13/2022_day_13.py
@@ -22,31 +22,36 @@ def read_input_file_as_flat_list(file_name: str = "input.txt") -> list[Packet]:
         return [json.loads(line_stripped) for line in f.read().splitlines() if (line_stripped := line.strip())]
 
 
-def is_packet_pair_in_order(packet_pair: PacketPair, source_of_truth_packet: int = 0) -> bool:
-    for i in range(len(packet_pair[source_of_truth_packet])):
+def is_packet_pair_in_order(left_packet: Packet, right_packet: Packet) -> bool | None:
+    for i in range(len(left_packet)):
         try:
-            left, right = packet_pair[0][i], packet_pair[1][i]
+            left, right = left_packet[i], right_packet[i]
             if isinstance(left, int) and isinstance(right, int):
                 if left != right:
                     return left < right
             else:
-                return is_packet_pair_in_order(
-                    (left if isinstance(left, list) else [left], right if isinstance(right, list) else [right]),
-                    source_of_truth_packet=source_of_truth_packet,
-                )
+                left_item = left if isinstance(left, list) else [left]
+                right_item = right if isinstance(right, list) else [right]
+                comparison_result = is_packet_pair_in_order(left_packet=left_item, right_packet=right_item)
+                if comparison_result is not None:
+                    return comparison_result
         except IndexError:
             return False
+    if len(left_packet) == len(right_packet):
+        return None  # packets are the same length and no comparison was made
     return True
 
 
 if __name__ == "__main__":
     print(
         "The sum of the indices of the packet pairs in the correct order is "
-        f"{sum([i + 1 for i, x in enumerate(read_input_file_as_pairs('input.txt')) if is_packet_pair_in_order(x)])}."
+        f"{sum([i + 1 for i, x in enumerate(read_input_file_as_pairs('input.txt')) if is_packet_pair_in_order(*x)])}."
     )
     sorted_packets = sorted(
         read_input_file_as_flat_list("input.txt") + [[[2]], [[6]]],  # add the two divider packets,
-        key=cmp_to_key(lambda x, y: -1 if is_packet_pair_in_order((x, y)) else 1),  # type: ignore  # mypy weirdness
+        key=cmp_to_key(  # type: ignore
+            lambda x, y: {True: -1, False: 1, None: 0}[is_packet_pair_in_order(left_packet=x, right_packet=y)]
+        ),
     )
     print(
         "The decoder key of the distress signal is "
