553c8b1a86a4b961c8da74d93c69cd6a6ed388f4
Nicholas de Paola <3079166+ndepaola@users.noreply.github.com>
2022-12-31 17:25:14 +1000

    2022 day 22 part 2

--- years/2022/22/2022_day_22.py
+++ years/2022/22/2022_day_22.py
@@ -4,7 +4,7 @@ from typing import Callable, TypeAlias
 Point: TypeAlias = tuple[int, int]  # x, y
 Map: TypeAlias = dict[Point, bool]  # point => is this point traversable (i.e. not a wall)?
 Commands: TypeAlias = list[str | int]  # str for direction to turn in, int for number of tiles to move
-EdgeAdjacencies: TypeAlias = dict[Point, Point]
+EdgeAdjacencies: TypeAlias = dict[tuple[Point, int], tuple[Point, int]]  # (point, direction) => (point, direction)
 
 DIRECTIONS: list[Point] = [(1, 0), (0, 1), (-1, 0), (0, -1)]  # east, south, west, north. top-left is (0, 0).
 
@@ -31,8 +31,21 @@ def read_input_file(file_name: str = "input.txt") -> tuple[Map, Commands]:
         return map_, commands
 
 
-def compute_edge_adjacencies(map_: Map) -> EdgeAdjacencies:
-    ...
+def get_edge_adjacencies() -> EdgeAdjacencies:
+    # TODO: hardcoding these mappings for my input for now. might come back to this puzzle later and solve it properly.
+    mapping = (
+        {((var_1, 49), 1): ((99, var_2), 2) for var_1, var_2 in zip(range(100, 149 + 1), range(50, 99 + 1))}
+        | {((149, var_1), 0): ((99, var_2), 2) for var_1, var_2 in zip(range(0, 49 + 1), range(149, 100 - 1, -1))}
+        | {((var_1, 0), 3): ((0, var_2), 0) for var_1, var_2 in zip(range(50, 99 + 1), range(150, 199 + 1))}
+        | {((var_1, 149), 1): ((49, var_2), 2) for var_1, var_2 in zip(range(50, 99 + 1), range(150, 199 + 1))}
+        | {((var_1, 100), 3): ((50, var_2), 0) for var_1, var_2 in zip(range(0, 49 + 1), range(50, 99 + 1))}
+        | {((50, var_1), 2): ((0, var_2), 0) for var_1, var_2 in zip(range(0, 49 + 1), range(149, 100 - 1, -1))}
+        | {((var_1, 0), 3): ((var_2, 199), 3) for var_1, var_2 in zip(range(100, 149 + 1), range(0, 49 + 1))}
+    )
+    reversed_mapping = {}  # ensure the edges can be traversed both ways
+    for key, value in mapping.items():
+        reversed_mapping[(value[0], (value[1] + 2) % len(DIRECTIONS))] = (key[0], (key[1] + 2) % len(DIRECTIONS))
+    return mapping | reversed_mapping
 
 
 def wrap_around_flat_map(map_: Map, position: Point, direction: int) -> tuple[Point, int]:
@@ -48,12 +61,20 @@ def wrap_around_flat_map(map_: Map, position: Point, direction: int) -> tuple[Po
     return position, direction  # the wrapped-around position is not traversable
 
 
+def wrap_around_cube_map(map_: Map, position: Point, direction: int) -> tuple[Point, int]:
+    adjacencies = get_edge_adjacencies()
+    assert (position, direction) in adjacencies.keys(), "edge adjacency map is incomplete"
+    if map_.get(adjacencies[(position, direction)][0], False) is True:
+        return adjacencies[(position, direction)]
+    return position, direction  # the wrapped-around position is not traversable
+
+
 def traverse(
     map_: Map, commands: Commands, wrap_around_callable: Callable[[Map, Point, int], tuple[Point, int]]
 ) -> int:
     position = min([x for x in map_.keys() if x[1] == 0])  # leftmost tile in top row
     direction = 0  # east
-    for command in commands:
+    for counter, command in enumerate(commands):
         if isinstance(command, str):
             # rotate
             direction = (direction + {"R": 1, "L": -1}[command]) % len(DIRECTIONS)
@@ -74,3 +95,4 @@ def traverse(
 if __name__ == "__main__":
     m, c = read_input_file("input.txt")
     print(f"The final password when the map is flat is {traverse(m, c, wrap_around_flat_map)}.")
+    print(f"The final password when the map is a cube is {traverse(m, c, wrap_around_cube_map)}.")
