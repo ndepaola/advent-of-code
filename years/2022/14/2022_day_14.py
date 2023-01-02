1c73164561e3c656a85959fbbfaa5138fbc9d03f
Nicholas de Paola <ndepaola97@gmail.com>
2023-01-02 23:11:36 +1000

    tweak 2022 day 14 implementation - use a set rather than dictionary

--- years/2022/14/2022_day_14.py
+++ years/2022/14/2022_day_14.py
@@ -1,28 +1,26 @@
 from typing import Iterable, TypeAlias
 
 Point: TypeAlias = tuple[int, int]  # x, y
-Cave: TypeAlias = dict[Point, bool]  # (x, y) => is this cell filled?
+Cave: TypeAlias = set[Point]  # (x, y) => is this cell filled?
 
 
-def draw_line(start: Point, end: Point) -> list[Point]:
+def draw_line(start: Point, end: Point) -> set[Point]:
     # super naive solution which handles drawing straight lines in cardinal directions only
-    return [
+    return {
         (x, y)
         for x in range(min(start[0], end[0]), max(start[0], end[0]) + 1)
         for y in range(min(start[1], end[1]), max(start[1], end[1]) + 1)
-    ]
+    }
 
 
 def read_input_file(file_name: str = "input.txt") -> Cave:
-    cave: Cave = {}
+    cave: Cave = set()
     with open(file_name, "r") as f:
         for line in f.readlines():
             split_line = line.strip().split(" -> ")  # handle each continuous line of rock separately
             for i in range(len(split_line) - 1):  # fill the cave between each adjacent pair of coordinates
                 start, end = split_line[i].split(","), split_line[i + 1].split(",")
-                cave = cave | {
-                    point: True for point in draw_line((int(start[0]), int(start[1])), (int(end[0]), int(end[1])))
-                }
+                cave |= draw_line((int(start[0]), int(start[1])), (int(end[0]), int(end[1])))
         return cave
 
 
@@ -33,24 +31,25 @@ def get_possible_sand_movements(point: Point) -> Iterable[Point]:
 
 
 def simulate_cave(cave: Cave, sand_source: Point, bottomless_void: bool) -> int:
-    sand_block_count, mutated_cave = 0, {**cave}
-    cave_height = max([x[1] for x in mutated_cave.keys()]) + 1
+    sand_block_count, mutated_cave = 0, {*cave}
+    cave_height = max([x[1] for x in mutated_cave]) + 1
     while True:
         sand_block = sand_source
-        if mutated_cave.get(sand_block, False):
+        if sand_block in mutated_cave:
             return sand_block_count  # the cave has filled with sand
-        mutated_cave[sand_block] = True
+        mutated_cave.add(sand_block)
         while True:
             exhausted_all_movement_options = True
             for new_point in get_possible_sand_movements(sand_block):
-                if mutated_cave.get(new_point, False) is False:
+                if new_point not in mutated_cave:
                     if new_point[1] > cave_height:
                         if bottomless_void:
                             return sand_block_count
                         else:
                             break
                     # the sand block can move into this position!
-                    mutated_cave[new_point], mutated_cave[sand_block] = True, False
+                    mutated_cave.remove(sand_block)
+                    mutated_cave.add(new_point)
                     sand_block = new_point
                     exhausted_all_movement_options = False
                     break
@@ -62,10 +61,7 @@ def simulate_cave(cave: Cave, sand_source: Point, bottomless_void: bool) -> int:
 if __name__ == "__main__":
     c = read_input_file("input.txt")
     print(
-        f"The number of sand blocks that can fall when the cave is a bottomless pit is "
-        f"{simulate_cave(c, (500, 0), bottomless_void=True)}."
+        f"After {simulate_cave(c, (500, 0), bottomless_void=True)} blocks of sand fall, any further blocks of sand "
+        f"will fall into the bottomless pit."
     )
-    print(
-        f"The number of sand blocks that can fall when the cave is not a bottomless pti is "
-        f"{simulate_cave(c, (500, 0), bottomless_void=False)}."
-    )
+    print(f"It takes {simulate_cave(c, (500, 0), bottomless_void=False)} blocks of sand to completely fill the cave.")
