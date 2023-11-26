ce59b273e9b1453e76deb0919caf568aa25b5112
Nicholas de Paola <ndepaola97@gmail.com>
2023-11-27 00:36:50 +1000

    some prep for working through advent of code in other languages :)

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ndepaola/advent-of-code
+
+go 1.21.4
---
+++ lib/lib.go
@@ -0,0 +1,62 @@
+package lib
+
+import (
+	"os"
+	"path"
+	"runtime"
+	"strconv"
+	"strings"
+)
+
+func ReadFile(fileName string) string {
+	// identify the location of the puzzle call site
+	// bit disgusting but w/e
+	var callSiteFileName string
+	var ok bool
+	i := 0
+	for {
+		_, callSiteFileName, _, ok = runtime.Caller(i)
+		if !ok {
+			panic("Could not find file name of caller")
+		}
+		if strings.Contains(callSiteFileName, "advent-of-code/years") {
+			break
+		} else {
+			i++
+		}
+	}
+
+	absolutePath := path.Join(path.Dir(callSiteFileName), fileName)
+	data, err := os.ReadFile(absolutePath)
+	if err != nil {
+		panic(err)
+	}
+	return strings.TrimRight(string(data), "\n")
+}
+
+type convert[T any] func(data string) T
+
+func ReadFileAsSliceOfType[T any](fileName string, fn convert[T]) []T {
+	split := strings.Split(ReadFile(fileName), "\n")
+	converted := make([]T, len(split))
+	for i := 0; i < len(split); i++ {
+		converted[i] = fn(split[i])
+	}
+	return converted
+}
+
+func ConvertToFloat64(data string) float64 {
+	parsed, err := strconv.ParseFloat(data, 64)
+	if err != nil {
+		panic(err)
+	}
+	return parsed
+}
+
+func ConvertToInt(data string) int {
+	parsed, err := strconv.Atoi(data)
+	if err != nil {
+		panic(err)
+	}
+	return parsed
+}
